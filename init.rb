df2442a78913b624796b747b0647797645376f87
Vincent Robert <vincent.robert@nanego.com>
2018-10-25 11:09:05 +0200

    Let admin choose if custom fields are displayed first or not

--- init.rb
+++ init.rb
@@ -7,8 +7,9 @@ Redmine::Plugin.register :redmine_customize_core_fields do
   author_url 'https://github.com/nanego'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
-  menu :admin_menu, :redmine_customize_core_fields, {:controller => 'core_fields', :action => 'index' }, :after => :custom_fields, :caption => :field_core_fields, html: { class: 'icon' }
-  settings :default => { 'override_issue_form' => false},
+  menu :admin_menu, :redmine_customize_core_fields, {:controller => 'core_fields', :action => 'index'}, :after => :custom_fields, :caption => :field_core_fields, html: {class: 'icon'}
+  settings :default => {'override_issue_form' => 'false',
+                        'display_custom_fields_first' => 'true'},
            :partial => 'settings/redmine_plugin_customize_core_fields'
 end
 
