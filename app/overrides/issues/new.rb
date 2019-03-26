db8c8b5a31fc5c66a54c29f6cee272c56716ec4f
Vincent Robert <vincent.robert@nanego.com>
2019-03-26 16:37:32 +0100

    Fix deface override

--- app/overrides/issues/new.rb
+++ app/overrides/issues/new.rb
@@ -1,6 +1,6 @@
 Deface::Override.new :virtual_path      => 'issues/new',
                      :name              => 'replace_default_new_form',
-                     :replace           => "erb[loud]:contains(\"render :partial => 'issues/form, :locals => {:f => f}'\")",
+                     :replace           => "erb[loud]:contains(\"render :partial => 'issues/form', :locals => {:f => f}\")",
                      :partial           => 'issues/customized_form'
 
 Deface::Override.new :virtual_path      => 'issues/_edit',
