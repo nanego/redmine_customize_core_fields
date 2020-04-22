3f3816be0d8736e0d34305ca57fbdc2ea32d9987
Vincent Robert <vincent.robert@nanego.com>
2020-04-22 22:26:51 +0200

    Add missing safe-attributes to CoreField model

---
+++ spec/controllers/core_fields_controller_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe CoreFieldsController, :type => :controller do
+
+  fixtures :roles #, :issues, :users, :issue_statuses, :projects, :projects_trackers, :trackers, :enumerations
+
+  before do
+    @request.session[:user_id] = 1 # Admin
+    Setting.default_language = 'en'
+    @field = CoreField.find_or_create_by!(identifier: 'description', visible: true)
+  end
+
+  it "should update core_fields params" do
+    expect(@field.visible).to be true
+    expect(@field.roles).to be_empty
+
+    put :update, params: {id: @field.identifier, core_field: {visible: '0', role_ids: ["1", "2"]}}
+    expect(response).to redirect_to edit_core_field_path(@field)
+
+    expect(@field.reload.visible).to be false
+    expect(@field.roles.size).to be 2
+  end
+
+end
