789b56b711dd48b9dc31430b93105dc50df9246c
Vincent Robert <vincent.robert@nanego.com>
2020-04-23 08:28:13 +0200

    Fix disabled_core_fields patch

--- lib/redmine_customize_core_fields/issue_patch.rb
+++ lib/redmine_customize_core_fields/issue_patch.rb
@@ -6,8 +6,9 @@ class Issue
     disabled_core_fields = tracker ? tracker.disabled_core_fields : []
     if self.project.present? && self.project.module_enabled?("customize_core_fields")
       disabled_core_fields | CoreField.not_visible(project).map(&:identifier).uniq
+    else
+      disabled_core_fields
     end
-    disabled_core_fields
   end
 
 end
