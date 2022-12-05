b5f4609f0989b4bcbc73b2ea5f6ff038298c0157
Vincent Robert <vincent.robert@nanego.com>
2022-12-05 15:30:21 +0100

    Fix patch include

--- lib/redmine_customize_core_fields/issue.rb
+++ lib/redmine_customize_core_fields/issue.rb
@@ -2,11 +2,11 @@ require_dependency 'issue'
 
 module RedmineCustomizeCoreFields
   module Issue
-    def disabled_core_fields(user=User.current)
+    def disabled_core_fields(user = User.current)
       disabled_core_fields = tracker ? tracker.disabled_core_fields : []
       disabled_core_fields | CoreField.not_visible_identifiers(project, user)
     end
   end
 end
 
-Issue.send(:include, RedmineCustomizeCoreFields::Issue)
+Issue.send(:prepend, RedmineCustomizeCoreFields::Issue)
