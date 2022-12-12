3b13cc4bd13ae0043e28e8eb70d659b3b165b761
Vincent Robert <vincent.robert@nanego.com>
2022-12-12 11:52:45 +0100

    Fix disabled fields patch

--- lib/redmine_customize_core_fields/issue.rb
+++ lib/redmine_customize_core_fields/issue.rb
@@ -4,7 +4,8 @@ module RedmineCustomizeCoreFields
   module Issue
     def disabled_core_fields(user = User.current)
       disabled_core_fields = tracker ? tracker.disabled_core_fields : []
-      disabled_core_fields | project.disabled_core_fields(user) if project.present?
+      disabled_core_fields |= project.disabled_core_fields(user) if project.present?
+      disabled_core_fields
     end
   end
 end
