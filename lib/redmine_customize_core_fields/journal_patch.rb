a5736e3f7a78434d8f288d91758733d0d7185150
Vincent <contact@vincent-robert.com>
2026-03-31 08:50:35 +0200

    Fix journal_patch corrupting journalize_changes

--- lib/redmine_customize_core_fields/journal_patch.rb
+++ lib/redmine_customize_core_fields/journal_patch.rb
@@ -3,9 +3,15 @@ module RedmineCustomizeCoreFields
 
     def visible_details(user = User.current)
       details = super(user)
-      return details if user.admin? || issue.nil?
+      return details if user.admin? || journalized_type != 'Issue'
 
-      disabled_fields = issue.disabled_core_fields(user)
+      # Important: use journalized (polymorphic association) instead of issue (belongs_to association)
+      # to avoid loading a fresh Issue from DB which would corrupt @custom_field_values
+      # in journalize_changes for subsequent saves on the same journal object.
+      current_issue = journalized
+      return details if current_issue.nil?
+
+      disabled_fields = current_issue.disabled_core_fields(user)
       details.reject { |detail|
         detail.property == 'attr' && disabled_fields.include?(detail.prop_key)
       }
